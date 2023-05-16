42230849e2f03ce38cb351d252eb81e89641cbb8
Abbas Nazari <mosbahsoftnazari@gmail.com>
2023-05-17 00:17:27 +0330

    fix & add intro adder in first of videos

--- telegramBot/__init__.py
+++ telegramBot/__init__.py
@@ -24,11 +24,7 @@ import message_conversation
 # Replace YOUR_API_ID and YOUR_API_HASH with your own values
 ApiId = 732757
 ApiHash = "9572884801dd15dcbb4ae2104ee26573"
-<<<<<<< HEAD
-app = Client("my_bot", api_id=ApiId, api_hash=ApiHash, bot_token="5520564422:AAEY6gRCAXlkTcvKZ-UinwGbvZggwLhAYFg")
-=======
-app = Client("my_bot", api_id=ApiId, api_hash=ApiHash, bot_token="5520564422:AAHyW_9W0cxlVmFh8b1FEbJOV65xtpize2w")
->>>>>>> 1778547d239babd54084329e251501223a8d91c9
+app = Client("my_bot", api_id=ApiId, api_hash=ApiHash, bot_token="5584893618:AAGocgNDat1Am7hjQCpXrX3qcTI3ojPgGi8")
 
 # welcome text  
 @app.on_message(filters.command('start'))
@@ -59,12 +55,7 @@ async def hello(client, message):
     #  other else message
     # 
 
-<<<<<<< HEAD
-app.run()
-# uvloop.install()
-=======
 # uvloop.install()
 
 
 app.run()
->>>>>>> 1778547d239babd54084329e251501223a8d91c9
--- telegramBot/functions/upload_video.py
+++ telegramBot/functions/upload_video.py
@@ -1,19 +1,40 @@
 import utils
 import os
 import ftplib
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import * 
+import cv2
+import ffmpeg
+import subprocess
+
 def upload_video(message, path, client):
     tag = utils.generate_string(20) + ".mp4"
     
-<<<<<<< HEAD
-    await message.reply("ویدیو شناسایی شد \n ویدیو در حال آپلود است \n شما میتوانید تگ زیر را ثبت کنید \n \n \n <pre>"+ tag + "</pre>   ",  quote=True)
-=======
     message.reply("ویدیو شناسایی شد \n ویدیو در حال آپلود است \n شما میتوانید تگ زیر را ثبت کنید \n \n \n <pre>"+ tag + "</pre>   ", quote=True)
->>>>>>> 1778547d239babd54084329e251501223a8d91c9
 
     video = message.video
     file_id = video.file_id
     npath = path + f"/files/videos/" + file_id + ".mp4"
     client.download_media(message=video, file_name=npath)
+    
+
+    wateMark =  path + f"/files/" + "water.mp4"
+    
+
+    command = 'ffmpeg -i '+wateMark+' -i '+npath+' -filter_complex "[0:v]scale=1366:768:force_original_aspect_ratio=1,pad=1366:768:-1:-1,setsar=1[v0]; [1:v]scale=1366:768:force_original_aspect_ratio=1,pad=1366:768:-1:-1,setsar=1[v1]; [v0][0:a][v1][1:a]concat=n=2:v=1:a=1[v][a]" -map "[v]" -map "[a]" -crf 18 -c:a aac -strict experimental ' + path + f"/files/videos/" + file_id + "-w.mp4" + '  -y ' 
+
+    try:
+        subprocess.check_output(command, shell=True)
+        print("FFmpeg command executed successfully.")
+        npath = path + f"/files/videos/" + file_id + "-w.mp4" ;
+    except subprocess.CalledProcessError as e:
+        print("Error executing FFmpeg command:", e)
+        npath = path + f"/files/videos/" + file_id + ".mp4" ;
+
+
+   
+   
+    
     
     file_ftp = open(npath,'rb')  
     
@@ -25,3 +46,4 @@ def upload_video(message, path, client):
     session.quit()
     # remove file from local
     os.remove(npath)
+    os.remove(npath.replace("-w.mp4" , ".mp4" ))
